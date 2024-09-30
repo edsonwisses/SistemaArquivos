1232ce77ccad54ee9ecf4eb587db83c9776f34ea
Edson Wisses <saonunca@gmail.com>
2024-09-30 14:38:29 -0300

    primeiro commit

---
+++ src/main/java/com/edsonwisses/Teste/SistemaArquivosApplication.java
@@ -0,0 +1,13 @@
+package com.edsonwisses.Teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaArquivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaArquivosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edsonwisses/Teste/SistemaArquivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsonwisses.Teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaArquivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
